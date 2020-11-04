8729f12c7eb732f7630ee191fd572ffe459fa41b
Brian Demro <demrobrian@hotmail.com>
2020-11-04 16:16:34 -0600

    updating done

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const massive = require('massive');
+const massive = require("massive");
+const ctrl = require("./products_controller");
 
 const app = express();
 
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive({
-    connectionString: CONNECTION_STRING,
-    ssl:{
-        rejectUnauthorized: false
-    }
-}).then(db => {
-    app.set('db', db);
-    console.log('Lookin good database')
-}).catch(err => console.log(err))
+connectionString: CONNECTION_STRING,
+ssl: {rejectUnauthorized: false}
+})
+  .then(db => {
+    app.set("db", db);
+    console.log('You done good kid!!')
+  })
+  .catch(err => console.log(err));
 
 app.use(express.json());
 
+app.post('/api/products', ctrl.create);
+app.get('/api/products', ctrl.getAll);
+app.get('/api/products/:id', ctrl.getOne);
+app.put('/api/products/:id', ctrl.update);
+app.delete('/api/products/:id', ctrl.delete);
+
 app.listen(SERVER_PORT, () => {
-    console.log (`Server listening on port ${SERVER_PORT}`);
+  console.log(`Server listening on port ${SERVER_PORT}.`);
 });
